0d33a367620ded33e7fdc2e6a7ba08a0960c1636
João Victor <joaovictor15308@gmail.com>
2026-07-26 12:39:33 -0300

    Adiciona painel de informações da data

--- frontend/src/routes/calendario/+page.svelte
+++ frontend/src/routes/calendario/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Calendar from './Calendar.svelte';
+	import Details from './Details.svelte';
 	import type { CalendarTask, CalendarDay } from './types.ts';
 
 	var dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
@@ -18,13 +19,18 @@
 		'December'
 	];
 
+	let selectedDay: CalendarDay = $state({
+		name: '',
+		enabled: true,
+		date: new Date()
+	});
 	let headers: string[] = dayNames;
 	let now = new Date();
 	let year = $state(now.getFullYear());
 	let month = $state(now.getMonth());
 	let eventText = $state('Click an item or date');
 
-	var days: CalendarDay[] = $derived(initMonth(year, month)); //	The days to display in each box
+	var days: CalendarDay[] = $derived(initMonth(year, month));
 
 	function randInt(max: number): number {
 		return Math.floor(Math.random() * max) + 1;
@@ -67,7 +73,6 @@
 			}
 		];
 
-		//This is where you calc the row/col to put each dated itemm
 		for (let i of items) {
 			let rc = findRowCol(i.date);
 			if (rc == null) {
@@ -94,26 +99,20 @@
 		let days = [];
 		let monthAbbrev = monthNames[month].slice(0, 3);
 		let nextMonthAbbrev = monthNames[(month + 1) % 12].slice(0, 3);
-		//	find the last Monday of the previous month
 		var firstDay = new Date(year, month, 1).getDay();
-		//console.log('fd='+firstDay+' '+dayNames[firstDay]);
 		var daysInThisMonth = new Date(year, month + 1, 0).getDate();
 		var daysInLastMonth = new Date(year, month, 0).getDate();
 		var prevMonth = month == 0 ? 11 : month - 1;
 
-		//	show the days before the start of this month (disabled) - always less than 7
 		for (let i = daysInLastMonth - firstDay; i < daysInLastMonth; i++) {
 			let d = new Date(prevMonth == 11 ? year - 1 : year, prevMonth, i + 1);
 			days.push({ name: '' + (i + 1), enabled: false, date: d });
 		}
-		//	show the days in this month (enabled) - always 28 - 31
 		for (let i = 0; i < daysInThisMonth; i++) {
 			let d = new Date(year, month, i + 1);
 			if (i == 0) days.push({ name: monthAbbrev + ' ' + (i + 1), enabled: true, date: d });
 			else days.push({ name: '' + (i + 1), enabled: true, date: d });
-			//console.log('i='+i+'  dt is '+d+' date() is '+d.getDate());
 		}
-		//	show any days to fill up the last row (disabled) - always less than 7
 		for (let i = 0; days.length % 7; i++) {
 			let d = new Date(month == 11 ? year + 1 : year, (month + 1) % 12, i + 1);
 			if (i == 0) days.push({ name: nextMonthAbbrev + ' ' + (i + 1), enabled: false, date: d });
@@ -129,8 +128,9 @@
 				d.getFullYear() === dt.getFullYear() &&
 				d.getMonth() === dt.getMonth() &&
 				d.getDate() === dt.getDate()
-			)
+			) {
 				return { row: Math.floor(i / 7) + 2, col: (i % 7) + 1 };
+			}
 		}
 		return null;
 	}
@@ -140,6 +140,7 @@
 	}
 	function dayClick(e: CalendarDay) {
 		eventText = 'onDayClick ' + JSON.stringify(e) + ' localtime=' + e.date.toString();
+		selectedDay = e;
 	}
 	function headerClick(e: string) {
 		eventText = 'onHheaderClick ' + JSON.stringify(e);
@@ -184,21 +185,31 @@
 			onClickHeader={(e: string) => headerClick(e)}
 		/>
 	</div>
+	<div class="event-container">
+		<Details {eventText} {selectedDay} />
+	</div>
 </main>
 
 <style>
 	main {
 		display: flex;
 		flex-direction: row;
-		width: 100vw;
+		justify-content: space-between;
+		width: 100%;
 	}
 	.calendar-container {
-		width: 70vw;
+		width: 70%;
 		overflow: hidden;
 		box-shadow: 0 2px 20px rgba(0, 0, 0, 0.1);
 		border-radius: 10px;
 		background: #fff;
-		max-width: 1200px;
+	}
+	.event-container {
+		width: 29%;
+		overflow: hidden;
+		box-shadow: 0 2px 20px rgba(0, 0, 0, 0.1);
+		border-radius: 10px;
+		background: #fff;
 	}
 	.calendar-header {
 		text-align: center;
---
+++ frontend/src/routes/calendario/Details.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	let { eventText, selectedDay } = $props();
+</script>
+
+<div class="details">
+	<h3 class="titulo">Evento:</h3>
+	<p>{eventText}</p>
+	<p>{selectedDay.date}</p>
+</div>
+
+<style>
+	.titulo {
+		width: 100%;
+		text-align: center;
+	}
+</style>
